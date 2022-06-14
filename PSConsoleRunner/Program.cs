66bdb6bbe4ece5809d918bfc316fb331d3fd984d
JMcCoard <JMcCoard@Steelcloud.com>
2022-06-14 11:02:55 -0400

    Adding inital configuration for testing PowerShell SDK making simple call for get host via a single file executable.

---
+++ PSConsoleRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Management.Automation;
+
+Console.WriteLine("Test PowerShell Runner!");
+
+var psInstance = PowerShell.Create();
+
+psInstance.AddScript("Get-Host");
+//psInstance.AddCommand("Get-Host");
+
+Console.WriteLine("Before Invoke.");
+var returnObj = psInstance.Invoke();
+Console.WriteLine("After Invoke.");
+
+Console.WriteLine("Name: " + returnObj.First().Members.First().Value);
+
+psInstance.Runspace?.Close();
+
+Console.WriteLine("Was successful run");
+
